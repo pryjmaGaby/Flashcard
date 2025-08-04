aa011aa46905de03881fb566341eee9ea7394341
pryjmaGaby <gabriella.pryjma@escola.pr.gov.br>
2025-08-04 12:24:41 -0300

    Update APP.js

--- APP.js
+++ APP.js
@@ -3,7 +3,7 @@ function criaCartao(categoria, pergunta, resposta) {
     let cartao = document.createElement('article')
     cartao.className = 'cartao'
 
-    cartao.innerHTML = `
+    cartao.innerHTML = 
     <div class="cartao__conteudo">
     <h3>${categoria}</h3>
     <div class="cartao__conteudo__pergunta">
@@ -13,7 +13,7 @@ function criaCartao(categoria, pergunta, resposta) {
         <p>${resposta}</p>
     </div>
     </div>
-    `
+    
 
     let respostaEstaVisivel = false
 
